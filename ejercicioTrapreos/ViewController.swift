96d49314db61ffd22be6289d60d19d471d9b6fd4
Lucas Romero Magaña <lucasromeromagana@gmail.com>
2022-01-19 11:43:58 +0100

    Initial Commit

---
+++ ejercicioTrapreos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicioTrapreos
+//
+//  Created by Lucas Romero Magaña on 19/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
